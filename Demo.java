f8da37f1e1ebe87eb008d556e8aed85877f14466
Sreeraj.R <vishnurajan789clappana@gmail.com>
2022-03-06 13:17:16 +0530

    file modified

--- Demo.java
+++ Demo.java
@@ -2,9 +2,7 @@ import java.io.*;
 
 class Demo{
 public static void main(String[] args){
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-System.out.println("Enter the number");
-int num = br.readLine();
-System.out.println("Entered number is "+num);
+System.out.println("Hello World");
+//Demo project
 }
 }
